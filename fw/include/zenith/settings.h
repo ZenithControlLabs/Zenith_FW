a316769f114bf5d2be2ba79e77fc49d9a6f78ca7
Julien de Castelnau <tripod320@gmail.com>
2025-10-26 16:33:11 +0100

    bump version

--- fw/include/zenith/settings.h
+++ fw/include/zenith/settings.h
@@ -16,7 +16,7 @@
 static const uint32_t SETTINGS_VER = 0x5002;
 
 #define ZTH_FW_MAJOR 1
-#define ZTH_FW_MINOR 3
+#define ZTH_FW_MINOR 4
 #define ZTH_FW_PATCH 0
 
 // A buffer for settings usable by any userland app using this library.
