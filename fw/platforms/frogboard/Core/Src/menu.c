0cb4fe8440200f1917ac18961437abccb337ac2f
Julien de Castelnau <tripod320@gmail.com>
2026-02-22 18:55:13 -0600

    timer based calibration & notch setting menu

--- fw/platforms/frogboard/Core/Src/menu.c
+++ fw/platforms/frogboard/Core/Src/menu.c
@@ -6,16 +6,21 @@
 #include "stick_types.h"
 #include <math.h>
 
+#define TIMER_BASED_CALIB
+
 struct {
   int cal_step;
   enum {
     MENU_MAIN,
     MENU_CALIB,
-    MENU_OUTPUTCAL
+    MENU_OUTPUTCAL,
+    MENU_NOTCH_SET
   } menu;
+  int set_calib_ind;
 } g_menu_state = {
   .cal_step = 0,
-  .menu = MENU_MAIN
+  .menu = MENU_MAIN,
+  .set_calib_ind = -1
 };
 
 ax_t reference_calibration_seq[] = {
@@ -56,12 +61,12 @@ bool get_menu_btn_press() {
     return btn_press_debounce == 0b00000001;
 }
 
-#define STICK_REPEAT_TIMER_INIT 2000
-#define STICK_REPEAT_TIMER_REPEAT 150
+#define STICK_REPEAT_TIMER_THRESH 2000
+#define STICK_REPEAT_TIMER_REPEAT 1000
 stick_menu_direction_t get_menu_dir_input(analog_data_t *an) {
   static stick_menu_direction_t last_input = DIR_NEUTRAL;
   static bool was_neutral = true;
-  static int repeat_timer = STICK_REPEAT_TIMER_INIT;
+  static int repeat_timer = STICK_REPEAT_TIMER_THRESH;
 
   stick_menu_direction_t menu_input = stick_val_to_menu_dir(an);
   
@@ -69,7 +74,7 @@ stick_menu_direction_t get_menu_dir_input(analog_data_t *an) {
   if ((menu_input & 0b100) && last_input == menu_input) {
     repeat_timer--;
   } else {
-    repeat_timer = STICK_REPEAT_TIMER_INIT;
+    repeat_timer = STICK_REPEAT_TIMER_THRESH;
   }
   was_neutral = was_neutral || menu_input == DIR_NEUTRAL;
   // Went back to neutral AND we transitioned from neutral/unknown to a direction
@@ -88,7 +93,12 @@ stick_menu_direction_t get_menu_dir_input(analog_data_t *an) {
 }
 
 bool calib_process(analog_data_t *raw, analog_data_t *cal) {
+  static uint32_t last_step_time = 0;
 
+  if (last_step_time == 0) {
+    last_step_time = HAL_GetTick();
+  }
+  
   int step = g_menu_state.cal_step;
   if ((step & 1) == 0) {
     cal->ax1 = reference_calibration_seq[0];
@@ -98,7 +108,11 @@ bool calib_process(analog_data_t *raw, analog_data_t *cal) {
     cal->ax2 = reference_calibration_seq[(step>>1)]; // y starts at 0
   }
 
+#ifdef TIMER_BASED_CALIB
+  if (HAL_GetTick() - last_step_time >= 2500) { // TODO: how many ticks ???
+#else
   if (get_menu_btn_press()) {
+#endif 
     analoglib_cal_advance(raw);
     step++;
     HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
@@ -106,11 +120,25 @@ bool calib_process(analog_data_t *raw, analog_data_t *cal) {
     HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
   }
 
+#ifdef TIMER_BASED_CALIB
+  else if (step >= 1 && get_menu_btn_press()) {
+    analoglib_cal_undo();
+    step--;
+    HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+    HAL_Delay(100);
+    HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+  }
+#endif
+
   if (step >= NUM_NOTCHES * 2) {
     g_menu_state.cal_step = 0;
+    last_step_time = 0;
     return true;
   } else {
-    g_menu_state.cal_step = step;
+    if (g_menu_state.cal_step != step) {
+      g_menu_state.cal_step = step;
+      last_step_time = HAL_GetTick();
+    }
     return false;
   }
 }
@@ -153,6 +181,12 @@ void factory_init(ADC_HandleTypeDef *hadc) {
   debug_print("FrogBoard Factory Init\n\r");
   settings_reset_to_factory();
 
+  uint16_t x = 0;
+  while (!get_menu_btn_press()) {
+    x += 16;
+    intf_out(x,x);
+  }
+
   // For now, factory calibration is just stick calibration
   bool done = false;
   uint16_t adc_res[2];
@@ -176,6 +210,10 @@ void factory_init(ADC_HandleTypeDef *hadc) {
   return;
 }
 
+void notch_calibrate(const ax_t in_points_x[], const ax_t in_points_y[],
+                     const ax_t notch_points_x[], const ax_t notch_points_y[],
+                     calib_results_t *calib_results);
+
 /**
   * @brief  Run menu routine.
   * @param  raw: input data read from ADC, in ax_t format
@@ -183,6 +221,7 @@ void factory_init(ADC_HandleTypeDef *hadc) {
   * @retval true if we are staying in settings mode
   */
 bool menu_process(analog_data_t *raw, analog_data_t *cal) {
+  static int16_t x_out, y_out = 0;
   switch (g_menu_state.menu) {
     case MENU_CALIB: {
       if (calib_process(raw, cal)) {
@@ -194,6 +233,52 @@ bool menu_process(analog_data_t *raw, analog_data_t *cal) {
       output_calib_process();
       g_menu_state.menu = MENU_MAIN;
       break;
+    }
+    case MENU_NOTCH_SET: {
+      bool press = get_menu_btn_press();
+      int ind = 0;
+      if (g_menu_state.set_calib_ind < 0) {
+        float ang = atan2f(cal->ax2, cal->ax1);
+        ind = (int)roundf(ang/M_PI_4);
+        ind = (ang < 0) ? ind + 8 : ind;
+        float mag = (cal->ax2)*(cal->ax2) + (cal->ax1)*(cal->ax1);
+        if (mag <= 0.3f) {
+          cal->ax1 = 0;
+          cal->ax2 = 0;
+          if (press) { 
+            g_menu_state.menu = MENU_MAIN;
+          }
+        } else {
+          if (press) {
+            // TODO: make it not work if the magnitude is in some deadzone?
+            g_menu_state.set_calib_ind = ind; 
+            x_out = AX_TO_INT16(g_settings.stick_config.notch_points_x[g_menu_state.set_calib_ind]);
+            y_out = AX_TO_INT16(g_settings.stick_config.notch_points_y[g_menu_state.set_calib_ind]);
+          }
+          ang = (float)ind * M_PI_4;
+          cal->ax1 = 0.7f * cosf(ang);
+          cal->ax2 = 0.7f * sinf(ang);
+        }
+      } else {
+        ind = g_menu_state.set_calib_ind;
+        stick_menu_direction_t menu_input = stick_val_to_menu_dir(cal);
+        x_out += (menu_input == DIR_RIGHT) ? 1 : ((menu_input == DIR_LEFT) ? -1 : 0);
+        y_out += (menu_input == DIR_UP) ? 1 : ((menu_input == DIR_DOWN) ? -1 : 0);
+        intf_out(x_out, y_out); 
+        if (press) {
+          g_settings.stick_config.notch_points_x[g_menu_state.set_calib_ind] = INT_N_TO_AX(x_out, 16);
+          g_settings.stick_config.notch_points_y[g_menu_state.set_calib_ind] = INT_N_TO_AX(y_out, 16);
+          notch_calibrate(g_settings.calib_results.notch_points_x_in,
+                        g_settings.calib_results.notch_points_y_in,
+                        g_settings.stick_config.notch_points_x,
+                        g_settings.stick_config.notch_points_y,
+                        &(g_settings.calib_results));
+          g_menu_state.set_calib_ind = -1;
+        }
+        cal->ax1 = INT_N_TO_AX(x_out, 16);
+        cal->ax2 = INT_N_TO_AX(y_out, 16);
+      }
+      
     }
     default:
     case MENU_MAIN: {
@@ -214,6 +299,10 @@ bool menu_process(analog_data_t *raw, analog_data_t *cal) {
         case DIR_UP: {
           debug_print("Leave settings mode...\n\r");
           return false;
+        }
+        case DIR_DOWN: {
+          g_menu_state.menu = MENU_NOTCH_SET;
+          break;
         }
         default: break;
       }
